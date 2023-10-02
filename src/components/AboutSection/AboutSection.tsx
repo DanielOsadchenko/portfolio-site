44f98a824964a50c308f136b2ee46fef7aae556a
Danylo Osadchenko <dm.osadchenko@gmail.com>
2023-10-02 16:51:00 +0200

    added education, certifications

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,9 +1,27 @@
+"use client"
 import Image from "next/image"
+import Masonry from 'react-masonry-css'
 import about from '../../../public/about.svg'
 import lang from '../../../public/lang.svg'
+import goit from '../../../public/goit.png'
+import udemy from '../../../public/udemy.png'
+import epam from '../../../public/epam.png'
+import college from '../../../public/college.png'
+import univercity from '../../../public/univercity.png'
+import education from '../../../public/education.svg'
+import certifications from '../../../public/certifications.svg'
 import SectionTitle from "../Titles/SectionTitle"
 import SectionSubTitle from "../Titles/SectionSubTitle"
 export default function AboutSection() {
+  const breakpoints = {
+    default: 2,
+    500: 1
+  }
+  const breakpointsCert = {
+    default: 3,
+    1100:2,
+    500: 1
+  }
   return (<section className="container mx-auto px-6 md:px-10 xl:px-20">
     <SectionTitle title={"About me"} number={2} id={"About"} />
     <div className="md:flex md:justify-between md:align-middle md:gap-5">
@@ -30,5 +48,53 @@ export default function AboutSection() {
       </ul>
     </div>
     <SectionSubTitle title={"Education"} number={2.2} />
+    <Masonry
+      breakpointCols={breakpoints}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      <div><Image src={education} alt="education" className="w-full"/></div>
+      
+      <div className="px-4 py-4 bg-cWhiteDark rounded-m">
+        <h3 className="mb-2 text-sm text-cVioletDark md:text-base font-medium">Zaporizhzhia National University</h3>
+        <h4 className="mb-2 text-sm text-cVioletDark md:text-base">Bachelor's Degree in Finance, Banking and Insurance</h4>
+        <Image src={univercity} alt="univercity diploma" loading="lazy" quality={80}/>
+      </div>
+
+     
+      <div className="px-4 py-4 bg-cWhiteDark rounded-m">
+        <h3 className="mb-2 text-sm text-cVioletDark md:text-base font-medium">College of Economics and Law</h3>
+        <h4 className="mb-2 text-sm text-cVioletDark md:text-base">Specialist in the Field of Finance</h4>
+        <Image src={college} alt="college diploma" loading="lazy" quality={80} />
+      </div>
+      
+       <div className="px-4 py-4 bg-cWhiteDark rounded-m">
+        <h3 className="mb-2 text-sm text-cVioletDark md:text-base font-medium">GoIT School</h3>
+        <h4 className="mb-2 text-sm text-cVioletDark md:text-base">Fullstack Developer (HTML, CSS, JavaScript, Node.js)</h4>
+        <Image src={goit} alt="goit diploma" loading="lazy" quality={80}/>
+      </div>
+
+    </Masonry>
+
+
+    <SectionSubTitle title={"Certifications"} number={2.3} />
+    <Masonry
+      breakpointCols={breakpointsCert}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      <div><Image src={certifications} alt="certifications" className="w-full"/></div>
+      
+      <div className="px-4 py-4 bg-cWhiteDark rounded-m text-center">
+        <h3 className="mb-2 text-sm text-cVioletDark md:text-base font-medium">Udemy Front-end Web Development Course</h3>
+        <Image src={udemy} alt="udemy diploma" loading="lazy" quality={80}/>
+      </div>
+
+      <div className="px-4 py-4 bg-cWhiteDark rounded-m text-center">
+        <h3 className="mb-2 text-sm text-cVioletDark md:text-base font-medium">EPAM IT Marathon</h3>
+        <Image src={epam} alt="epam diploma" loading="lazy" quality={80} />
+      </div>
+
+    </Masonry>
     </section>)
 }
